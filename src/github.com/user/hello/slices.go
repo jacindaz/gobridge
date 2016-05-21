24875208881427161ebc98bdbd970060b53e2596
Jacinda <jacindaz@gmail.com>
2016-05-21 14:19:37 -0400

    Practice with slices and adding items to my slice

---
+++ src/github.com/user/hello/slices.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+    my_slice := make([][]int, 0)
+    fmt.Println(my_slice)
+
+    my_nested_slice := make([]int, 0)
+    my_nested_slice = append(my_nested_slice, 34)
+    my_nested_slice = append(my_nested_slice, 12)
+
+    my_other_nested_slice := []int{56,78,90}
+
+    my_slice = append(my_slice, my_nested_slice)
+    my_slice = append(my_slice, my_other_nested_slice)
+    fmt.Println(my_slice)
+
+    my_int_slice := make([]int, 0)
+    for i := 0; i < 10; i++ {
+        my_int_slice = append(my_int_slice, i + 1)
+    }
+
+    fmt.Println(my_int_slice)
+    fmt.Println(my_int_slice[0:5])
+}
