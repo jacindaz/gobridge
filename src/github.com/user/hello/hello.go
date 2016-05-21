c92874de962f868978749ea1e584184b437ec143
Jacinda <jacindaz@gmail.com>
2016-05-21 10:36:21 -0400

    Can declare 2 vars if they are the same type

--- src/github.com/user/hello/hello.go
+++ src/github.com/user/hello/hello.go
@@ -3,9 +3,8 @@ package main
 import "fmt"
 
 func main() {
-    var ten int = 10
-    var five int = 5
-    var true_true = true
+    var ten, five int = 10, 5
+    var true_true, false_false = true, false
     var random_string = "blah blah"
 
     fmt.Printf("hello, world\n")
@@ -14,9 +13,9 @@ func main() {
 
 
     fmt.Println("\nPrintln string interpolation of a boolean: %t", true_true)
-    fmt.Println("Println string interpolation of a boolean: %v", true_true)
+    fmt.Println("Println string interpolation of a boolean: %v", false_false)
 
     fmt.Printf("\nPrintf string interpolation of a boolean: %t", true_true)
-    fmt.Printf("\nPrintf string interpolation of a boolean: %v", true_true)
+    fmt.Printf("\nPrintf string interpolation of a boolean: %v", false_false)
 
 }
