26f526c70b0d995f0849f2edbcbd7dfc216074c8
Jacinda <jacindaz@gmail.com>
2016-05-21 13:29:11 -0400

    Print out errors, experiment with error package

--- src/github.com/user/hello/hello.go
+++ src/github.com/user/hello/hello.go
@@ -1,6 +1,9 @@
 package main
 
 import "fmt"
+import "reflect"
+import "errors"
+// import "time"
 
 func main() {
     if num := 9; num == 9 {
@@ -9,4 +12,28 @@ func main() {
     } else {
         fmt.Println("else statement")
     }
+
+    am_i_an_error := true
+    the_error := simulating_errors(am_i_an_error)
+
+    var error_to_print = fmt.Errorf("You passed in %t as the argument: ", am_i_an_error)
+    fmt.Print(error_to_print)
+    fmt.Println(reflect.TypeOf(the_error))
 }
+
+func simulating_errors(am_i_an_error bool) error {
+    err := errors.New("such corruption, much error")
+
+    if am_i_an_error {
+        return err
+    } else {
+        return nil
+    }
+}
+
+// func error_formatting(the_error error, am_i_an_error bool) {
+//     var error_to_print = fmt.Errorf("You passed in %t as the argument: ", am_i_an_error)
+//     fmt.Print(error_to_print)
+
+//     fmt.Println(reflect.TypeOf(the_error))
+// }
