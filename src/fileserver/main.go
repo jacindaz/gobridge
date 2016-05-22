aac72ba358f5e35595406783838d72fa886d780c
Jacinda <jacindaz@gmail.com>
2016-05-22 14:37:22 -0400

    Added port number for Heroku and some print statements for console

--- src/fileserver/main.go
+++ src/fileserver/main.go
@@ -2,17 +2,26 @@ package main
 
 import (
     "net/http"
-
+    "fmt"
     "github.com/russross/blackfriday"
+    "os"
 )
 
 func main() {
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
+
     http.HandleFunc("/markdown", generateMarkdown)
     http.Handle("/", http.FileServer(http.Dir("public")))
-    http.ListenAndServe(":8000", nil)
+    http.ListenAndServe(":"+port, nil)
 }
 
 func generateMarkdown(rw http.ResponseWriter, r *http.Request) {
+    fmt.Printf("\nI am r: %v", r)
+    fmt.Printf("\nI am r: %v", r.FormValue)
+
     markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
     rw.Write(markdown)
 }
