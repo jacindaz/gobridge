8ad8a3c23dbdfd5c26964256c3d28a24ed1c1882
Jacinda <jacindaz@gmail.com>
2016-05-22 08:41:17 -0400

    Function with multiple return values, all of different types

--- src/github.com/user/hello/functions.go
+++ src/github.com/user/hello/functions.go
@@ -4,6 +4,9 @@ import "fmt"
 func main() {
     my_function_with_all_the_same_types("Hello", "Jacinda", "blah")
     my_func_with_different_types("Jacinda", 26)
+
+    some_int, some_bool, some_string := multiple_return_values()
+    fmt.Printf("\n\nsome_int: %d, some_bool: %t, some_string: %s", some_int, some_bool, some_string)
 }
 
 func my_function_with_all_the_same_types(str1, str2, str3 string) {
@@ -14,4 +17,8 @@ func my_function_with_all_the_same_types(str1, str2, str3 string) {
 
 func my_func_with_different_types(name string, age int) {
     fmt.Printf("My name is %s and I am %d years old", name, age)
+}
+
+func multiple_return_values() (int, bool, string) {
+    return 100, false, "some string"
 }
