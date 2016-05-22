18ac4dd3414611acb6ebd5e5a25e831aca63fbd7
Jacinda <jacindaz@gmail.com>
2016-05-22 00:03:39 -0400

    Added another syntax for a range for loop

--- src/github.com/user/hello/ranges.go
+++ src/github.com/user/hello/ranges.go
@@ -5,6 +5,10 @@ func main() {
     my_array := [10]int{1,2,3,4,5,6,7,8,9,10}
     fmt.Println(my_array)
 
+    for _, num := range my_array {
+        fmt.Println(num)
+    }
+
     for i, num := range my_array {
         if my_array[i] % 2 == 0 {
             fmt.Printf("\n%d is ", num)
