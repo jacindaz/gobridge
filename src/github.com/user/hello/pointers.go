feaf40581b120248df3c13e6bcbc62ac0a8fe79d
Jacinda <jacindaz@gmail.com>
2016-05-22 11:00:41 -0400

    Go by example pointers, printing pointer memory address and pointer values

--- src/github.com/user/hello/pointers.go
+++ src/github.com/user/hello/pointers.go
@@ -23,3 +23,31 @@ func main() {
     ptr_to_ptr := &another_variable_ptr
     fmt.Println("pointer to a pointer: ", &ptr_to_ptr)
 }
+
+func zeroval(ival int) {
+    ival = 0
+}
+
+func zeroptr(iptr *int) {
+    *iptr = 0
+}
+
+func go_by_example_pointer_code() {
+    i := 1
+    fmt.Println("initial for i:", i)
+    zeroval(i)
+    fmt.Println("zeroval for i:", i)
+    zeroptr(&i)
+    fmt.Println("zeroptr for i:", i)
+    fmt.Println("pointer for i:", &i)
+    fmt.Println(reflect.TypeOf(&i))
+
+    i = 2
+    fmt.Println("zeroptr for i:", i)
+    fmt.Println("pointer for i:", &i)
+
+    j := 2
+    zeroptr(&j)
+    fmt.Println("zeroptr for j:", j)
+    fmt.Println("pointer for j:", &j)
+}
